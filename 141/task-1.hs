75b4253544a37bab357d1f96413b2994ddd5bbe3
Niko Lepka <slench102+git@gmail.com>
2021-12-05 19:50:21 +0100

    Update task-1.hs

--- 141/task-1.hs
+++ 141/task-1.hs
@@ -1,11 +1,18 @@
-import Control.Arrow ((&&&))
-
 findDivisors :: Int -> [Int]
 findDivisors 0 = []
 findDivisors n = filter (\x -> n `mod` x == 0) [1..n]
 
-findAllIntegersWithNDivisors :: Int -> [(Int, [Int])]
+{- Finds all the divisors of all integers and filters the list 
+ - by the required divisor length.
+ - Since a number is always its own divisor, 
+ - we just strip all but the very last integer in the divisor list.
+ -}
+findAllIntegersWithNDivisors :: Int -> [Int]
 findAllIntegersWithNDivisors len = 
-    filter (\x -> length (snd x) == len) $ (id &&& findDivisors) <$> [1..]
+    -- need to use fmap instead of <$> because <$> isn't infixr, but instead infixl.
+    fmap last . filter (\x -> length x == len) $ findDivisors <$> [1..]
 
 solution = take 10 $ findAllIntegersWithNDivisors 8
+
+-- a colollary to the comment above, is that any number with exactly 2 divisors must be prime.
+primes = take 10 $ findAllIntegersWithNDivisors 2
