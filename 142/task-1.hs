212e2b77063555ec4f7dd5fd3fa4acd98399b9a1
Niko Lepka <slench102+git@gmail.com>
2021-12-06 10:33:06 +0100

    Update task-1.hs

--- 142/task-1.hs
+++ 142/task-1.hs
@@ -5,10 +5,8 @@
 divisors :: Int -> [Int]
 divisors n = filter (\x -> n `mod` x == 0) [1..n]
 
-n = 2
-m = 42
-
-result = filter (\x -> x `mod` 10 == n) $ divisors 42
-
 main :: IO ()
-main = print result
+main = do 
+    let (m, n) = (42, 2)
+        result = filter (\x -> x `mod` 10 == n) $ divisors 42
+    print result
